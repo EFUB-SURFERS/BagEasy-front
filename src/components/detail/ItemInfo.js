7f139967fbecd9c1a01248b29dc4e57c0f8a76e1
jiwoorld <bncmdbev@gmail.com>
2023-07-07 00:15:41 +0900

    ✨ ItemInfo 이미지 추가

--- src/components/detail/ItemInfo.js
+++ src/components/detail/ItemInfo.js
@@ -1,7 +1,17 @@
 import styled from "styled-components";
 
+import picture from "../../assets/picture.png";
+
 const ItemInfo = () => {
-  return <Div></Div>;
+  return (
+    <Div>
+      <ItemImages src={picture} />
+      <SellerInfo />
+      <ItemContent />
+      <Comment />
+      <ItemDetail />
+    </Div>
+  );
 };
 export default ItemInfo;
 
@@ -11,4 +21,20 @@ const Div = styled.div`
   width: 100%;
 
   display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
 `;
+
+const ItemImages = styled.img`
+  width: 390px;
+  height: 390px;
+`;
+
+const ItemContent = styled.div``;
+
+const SellerInfo = styled.div``;
+
+const Comment = styled.div``;
+
+const ItemDetail = styled.div``;
