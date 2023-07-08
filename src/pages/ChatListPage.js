8ddada665598353284a38bcdb39c9f88e4573952
jimin song <songing01@gmail.com>
2023-07-08 23:07:39 +0900

    💄 헤더 공통 컴포넌트 폴더로 이동

--- src/pages/ChatListPage.js
+++ src/pages/ChatListPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "../components/ChatList/Header.js";
+import Header from "../components/Common/Header.js";
 import List from "../components/ChatList/List.js";
 const ChatListPage = () => {
   return (
