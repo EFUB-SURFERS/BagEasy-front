a83e1fd584936f4ea8b9e3d12746cb9e101489c3
jimin song <songing01@gmail.com>
2023-07-17 20:54:42 +0900

    ✨ API 요청 함수 default instance 생성

--- src/api/posts.js
+++ src/api/posts.js
@@ -1,16 +1,12 @@
-import axios from "axios";
+import client from "./client";
 //판매글 관련 api 요청 함수 여기에 구현해주세요
 
 //판매글 구매 확정 api
-//client.js에 default axios instance 구현시 수정
 export const FinishDeal = async (postId, buyerId) => {
   try {
-    const res = await axios.put(
-      `http://localhost:8080/posts/${postId}/isSold`,
-      {
-        buyerId: { buyerId },
-      },
-    );
+    const res = await client.put(`posts/${postId}/isSold`, {
+      buyerId: { buyerId },
+    });
 
     console(res);
   } catch (err) {
