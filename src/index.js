d31a6fb3d02fe998f379b17c87180b22c67fa0b4
LJH <049494@ewhain.net>
2023-07-18 14:32:49 +0900

    📝 클라이언트 id env 저장

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/global.css";
 import { BrowserRouter } from "react-router-dom";
-import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
