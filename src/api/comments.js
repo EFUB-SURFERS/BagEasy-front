ec66d57b5ae6e57511fdb46c65c0ee60d7edf75f
Jeongmin Cho <jojm0829@gmail.com>
2023-07-18 20:05:39 +0900

    ✨ 댓글 삭제 api 추가
    src/api/comments.js에 댓글 삭제 api 추가

--- src/api/comments.js
+++ src/api/comments.js
@@ -1,8 +1,21 @@
 import client from "./client";
 
+//댓글 조회 api
 export const getComments = async postId => {
   try {
     const res = await client.get(`posts/${postId}/comments`);
+
+    console.log(res.data);
+    return res.data;
+  } catch (err) {
+    console.log("에러 발생", err);
+  }
+};
+
+//댓글 삭제 api
+export const deleteComments = async commentId => {
+  try {
+    const res = await client.delete(`comments/${commentId}`);
 
     console.log(res.data);
     return res.data;
