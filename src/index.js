10ed6f35b90f0bdb7fdcd45e11d6570abef85d1b
LJH <049494@ewhain.net>
2023-07-18 14:24:25 +0900

    🐛 구글 로그인 api 연결 오류 수정, 로딩 페이지 추가

--- src/index.js
+++ src/index.js
@@ -7,9 +7,7 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_AUTH_CLIENT_ID}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </GoogleOAuthProvider>,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
 );
