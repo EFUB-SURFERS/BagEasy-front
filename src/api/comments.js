123c3d239d3bc4d04d1e76f67a2b26ff56719328
Jeongmin Cho <jojm0829@gmail.com>
2023-07-19 16:15:36 +0900

    ✨ Feat: 댓글 작성 api 추가
    src/api/comments.js에 댓글 작성 api 추가

--- src/api/comments.js
+++ src/api/comments.js
@@ -1,5 +1,19 @@
 import client from "./client";
 
+//댓글 작성 api
+export const writeComment = async (postId, body) => {
+  try {
+    const res = await client.post(`posts/${postId}/comments`, body, {
+      headers: { "Content-Type": "application/json" },
+    });
+
+    console.log(res.data);
+    return res.data;
+  } catch (err) {
+    console.log("에러 발생", err);
+  }
+};
+
 //댓글 조회 api
 export const getComments = async postId => {
   try {
