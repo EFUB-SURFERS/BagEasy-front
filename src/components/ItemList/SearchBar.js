2a7d8343bcd553c29bafc1c3ab4986159dfee58d
Jeongmin Cho <jojm0829@gmail.com>
2023-07-22 21:29:46 +0900

    💄 Design: 메인페이지 상단의 학교 검색 디자인 변경
    인풋창을 가로로 꽉 채우고, 토글 버튼은 그 밑 좌측에 위치시킴.

--- src/components/ItemList/SearchBar.js
+++ src/components/ItemList/SearchBar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import searchImg from "../../assets/searchImg.png";
 import styled from "styled-components";
 import Toggle from "./Toggle";
@@ -7,13 +7,19 @@ import Modal from "./../UpdateUni/Modal";
 const SearchBar = ({ onToggle, filter }) => {
   const [isOpen, setIsOpen] = useState(false);
   const [uni, setUni] = useState("");
+  const [uniDisplay, setUniDisplay] = useState();
+  useEffect(() => {
+    if (!isOpen) setUniDisplay(uni);
+  }, [isOpen]);
   return (
     <SearchWrapper>
+      <Bar onClick={() => setIsOpen(true)}>
+        <Text>{uni}</Text>
+        <SearchBtn>
+          <SearchImg src={searchImg} />
+        </SearchBtn>
+      </Bar>
       <Toggle onToggle={onToggle} filter={filter} />
-      <University>{isOpen ? "" : uni}</University>
-      <SearchBtn onClick={() => setIsOpen(true)}>
-        <SearchImg src={searchImg} />
-      </SearchBtn>
       {isOpen ? (
         <Modal
           isOpen={isOpen}
@@ -27,27 +33,41 @@ const SearchBar = ({ onToggle, filter }) => {
 };
 
 const SearchWrapper = styled.div`
+  /* border: 1px solid red; */
   display: flex;
-  align-items: center;
+  flex-direction: column;
   justify-content: space-between;
-  height: 43px;
+  height: 103px;
+  box-sizing: border-box;
+  background: white;
   margin-top: 97px;
   position: fixed;
   width: 100%;
-  background: white;
+  padding: 0 19px;
+  padding-bottom: 13px;
 `;
 
-const University = styled.div`
+const Bar = styled.div`
+  display: flex;
+  align-items: center;
+  background: #efefef;
+  border-radius: 20px;
+  height: 39px;
+`;
+
+const Text = styled.div`
   text-align: center;
   font-weight: bold;
-  font-size: 14px;
+  font-size: 16px;
+  margin-left: 12px;
 `;
 
 const SearchBtn = styled.div`
   width: 1.7rem;
   right: 1rem;
   transform: translateY(2px);
-  margin-right: 0.5rem;
+  margin-right: 10px;
+  margin-left: auto;
 `;
 
 const SearchImg = styled.img`
