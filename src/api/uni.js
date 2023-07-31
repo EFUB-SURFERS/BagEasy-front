b6e8ac79a9153b7018ce8efeac7bf52901145a7f
jimin song <songing01@gmail.com>
2023-08-01 02:08:28 +0900

    🐛 학교 API url 수정

--- src/api/uni.js
+++ src/api/uni.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 export const GetUniList = async uni => {
   try {
-    const res = await axios.get(
-      `http://universities.hipolabs.com/search?name=${uni}`,
-    );
+    const res = await axios.get(`https://hipo.bageasy.net/search?name=${uni}`);
 
     return res.data;
   } catch (err) {
