63abb8a48b12d572e0e56a317745c7ec62d7fc33
Jeongmin Cho <jojm0829@gmail.com>
2023-08-01 15:11:12 +0900

    📝 Chore: mockReplies 삭제

--- src/components/Comment/ReplyList.js
+++ src/components/Comment/ReplyList.js
@@ -3,23 +3,6 @@ import styled from "styled-components";
 import { getReplies } from "../../api/replies";
 import Comment from "./Comment";
 
-const mockReplies = [
-  {
-    replyId: 2,
-    commentId: 2,
-    memberId: 1,
-    replyContent: "대댓글..!!!!!",
-    isSecret: true,
-  },
-  {
-    replyId: 2,
-    commentId: 2,
-    memberId: 1,
-    replyContent: "헤헤 된당",
-    isSecret: true,
-  },
-];
-
 const ReplyList = ({
   commentId,
   setReplying,
