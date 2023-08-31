728c98227d362503ac70d88249ec02d47ae7925e
jimin song <songing01@gmail.com>
2023-08-31 21:49:12 +0900

    💄 거래확정 모달 텍스트 색상 변경

--- src/components/ChatRoom/Modal.js
+++ src/components/ChatRoom/Modal.js
@@ -87,7 +87,7 @@ const Container = styled.div`
   transform: translate(-50%, -50%);
 
   .title {
-    color: #383200;
+    color: #0e312b;
     width: 65%;
     margin-left: 8%;
     margin-top: 22px;
