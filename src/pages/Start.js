e45e717c9521c56c3dec1470d96ee178a6d944c6
LJH <049494@ewhain.net>
2023-10-26 23:41:08 +0900

    style : 시작 페이지 퍼블리싱

--- src/pages/Start.js
+++ src/pages/Start.js
@@ -1,8 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import logo from "../assets/StartPage/logo.png";
-import title from "../assets/StartPage/title.png";
-import newLogo from "../assets/common/newLogo.png";
+import start from "../assets/StartPage/start.png";
 import { useNavigate } from "react-router-dom";
 
 const Start = () => {
@@ -10,15 +8,15 @@ const Start = () => {
 
   return (
     <StartScreenContainer>
-      <Logo>
-        <NewLogoImg src={newLogo} alt="newLogo" />
-        <LogoImg src={logo} alt="logo" />
-        <TitleImg src={title} alt="title" />
-      </Logo>
       <Button onClick={() => navigate("/login")}>시작하기</Button>
+      <BG src={start} />
     </StartScreenContainer>
   );
 };
+
+const BG = styled.img`
+  height: 100vh;
+`;
 
 const StartScreenContainer = styled.div`
   display: flex;
@@ -26,29 +24,7 @@ const StartScreenContainer = styled.div`
   align-items: center;
   justify-content: flex-start;
   height: 100vh;
-`;
-
-const NewLogoImg = styled.img`
-  width: 50px;
-  margin-bottom: 30px;
-`;
-
-const LogoImg = styled.img`
-  width: 60%;
-  margin-bottom: 75px;
-`;
-
-const TitleImg = styled.img`
-  width: 50%;
-`;
-
-const Logo = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  margin-bottom: 9rem;
-  flex: 1;
+  background-color: #0e312b;
 `;
 
 const Button = styled.button`
@@ -56,14 +32,13 @@ const Button = styled.button`
   padding: 10px 20px;
   width: 327px;
   font-size: 20px;
-  background-color: #ffc700;
-  color: #ffffff;
+  background-color: white;
   font-weight: bolder;
   border: none;
   border-radius: 30px;
   cursor: pointer;
   position: absolute;
-  bottom: 7rem;
+  bottom: 6rem;
 `;
 
 export default Start;
